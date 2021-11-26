fafefb27887d2f89dc76b92bf6c9b519efaee6db
Brooklyn92 <Tubanar173russia>
2021-11-26 16:27:18 +0400

    ParameterizedTest default

--- src/test/java/lesson5/ParametersTest.java
+++ src/test/java/lesson5/ParametersTest.java
@@ -3,8 +3,8 @@ package lesson5;
 import io.qameta.allure.Allure;
 import lesson5.steps.WebSteps;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+//import org.junit.jupiter.params.ParameterizedTest;
+//import org.junit.jupiter.params.provider.ValueSource;
 
 public class ParametersTest extends TestBase {
 
@@ -12,8 +12,8 @@ public class ParametersTest extends TestBase {
     private WebSteps steps=new WebSteps();
 
     @DisplayName("Проверка города")
-    @ParameterizedTest(name = "{displayName} {0}")
-    @ValueSource(strings = {"Санкт-Петербург","Москва"} )
+    //@ParameterizedTest(name = "{displayName} {0}")
+    //@ValueSource(strings = {"Санкт-Петербург","Москва"} )
     public void testLambda(String city){
         Allure.parameter("Город", city);
     }
