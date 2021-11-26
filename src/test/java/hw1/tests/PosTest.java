a5222f16843cb0342ff0f0abf4a9a2bca46e88e7
Brooklyn92 <Tubanar173russia>
2021-11-26 17:13:41 +0400

    new test class

---
+++ src/test/java/hw1/tests/PosTest.java
@@ -0,0 +1,12 @@
+package hw1.tests;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class PosTest {
+    @Test
+    public void posTest1(){
+        assertTrue(true);
+    }
+}
