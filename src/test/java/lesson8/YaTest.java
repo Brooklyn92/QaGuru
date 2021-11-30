bbf8fbbcd7da949d5bd26bb5e7671c9fe7c2db5d
Brooklyn92 <Tubanar173russia>
2021-11-30 13:08:09 +0400

    add annotations and method

--- src/test/java/lesson8/YaTest.java
+++ src/test/java/lesson8/YaTest.java
@@ -1,8 +1,13 @@
 package lesson8;
+import com.codeborne.selenide.CollectionCondition;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-import static com.codeborne.selenide.Selenide.open;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static com.codeborne.selenide.CollectionCondition.texts;
+import static com.codeborne.selenide.Selenide.*;
 
 public class YaTest {
 
@@ -11,7 +16,20 @@ public class YaTest {
     @Test
     void selenideSearchTest() {
         open("https://ya.ru/");
+        $("#text").setValue("Selenide");
+        $("#text").pressEnter();
+        $$("#search-result").shouldHave(texts("Selenide"));
+    }
 
+    @ValueSource(strings = {"Selenide","Allure"})//прописываются параметры. Тест будет пройден столько раз, сколько указано параметров
+    @Tag("higth")
+    @DisplayName("Search in Yandex")
+    @ParameterizedTest(name = "Search in ya.ru word {0}") //в {} прописывается индекс параметра. Параметр search имеет индекс 0
+    void CommonYaSearchTest(String search) {
+        open("https://ya.ru/");
+        $("#text").setValue(search);
+        $("#text").pressEnter();
+        $$("#search-result").shouldHave(texts(search));
     }
 }
 
