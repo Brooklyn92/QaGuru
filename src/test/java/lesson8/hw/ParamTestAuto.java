9532dcecfdfe24cf9123dae225361baab5cc2b87
Brooklyn92 <Tubanar173russia>
2021-12-01 15:42:46 +0400

    Hw part1

--- src/test/java/lesson8/hw/ParamTestAuto.java
+++ src/test/java/lesson8/hw/ParamTestAuto.java
@@ -1,15 +1,18 @@
 package lesson8.hw;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.selector.ByText;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byName;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
-public class ParamTestKinopoisk {
+public class ParamTestAuto {
 
     @BeforeAll
     public static void set(){
@@ -17,11 +20,11 @@ public class ParamTestKinopoisk {
         Configuration.holdBrowserOpen=true;
     }
     @Test
-    public void testKP(){
-        open("https://www.kinopoisk.ru/");
-        $(By.name("kp_query")).setValue("Аркейн").pressEnter();
-        $(".block_left_pad").shouldHave(text("Аркейн (сериал)"));
-
-
+    public void testRiot(){
+        open("https://arcane.com/ru-ru/");
+        $("#riotbar-right-content").shouldHave(text("Вход")).click();
+        $(byName("username")).setValue("Riot73");
+        $(byName("password")).setValue("Riot73");
+        $("button[type='submit']").click();
     }
 }
