b77a19d2c5a52785791a2a88b0abb432da8f10e7
Brooklyn92 <Tubanar173russia>
2021-12-02 16:33:43 +0400

    Hw part4

--- src/test/java/lesson8/hw/ParamTestAuto.java
+++ src/test/java/lesson8/hw/ParamTestAuto.java
@@ -3,6 +3,8 @@ package lesson8.hw;
 import com.codeborne.selenide.CollectionCondition;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import static com.codeborne.selenide.Selectors.byName;
@@ -16,12 +18,14 @@ public class ParamTestAuto {
         Configuration.holdBrowserOpen=true;
     }
 
-  @ParameterizedTest
+  @ParameterizedTest(name = "Ищем сериалы в кинопоиске {0} и проверка отображения заголова серила {1}")
    @CsvSource( value = {
            "Аркейн | Аркейн (сериал)",
            "Во все тяжкие | Во все тяжкие (сериал)"
    },
         delimiter = '|')
+  @Tag("hight")
+  @DisplayName("Поиск на сайте Kinopoisk")
     public void testKP(String film, String title){
         open("https://www.kinopoisk.ru/");
         $(byName("kp_query")).setValue(film).pressEnter();
