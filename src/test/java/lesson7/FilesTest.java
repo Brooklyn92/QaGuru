b6ab1a55d98dd3b6434be6f3b5555aca75fd9bb7
Brooklyn92 <Tubanar173russia>
2021-12-15 17:31:29 +0400

    lesson7 part2

--- src/test/java/lesson7/FilesTest.java
+++ src/test/java/lesson7/FilesTest.java
@@ -1,17 +1,36 @@
 package lesson7;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class FilesTest {
 
     @Test
-    void fileNameShouldDisplayedAfterUploadActionTest() {
+    @DisplayName("Загрузка файла по абсолютному пути(не рекомендуется)")
+    void fileNameShouldDisplayedAfterUploadActionAbsolutePathTest() {
         open("https://the-internet.herokuapp.com/upload");
-        $("input[type='file']").uploadFile();
-
+       File exampleFile1 =  new File("C:\\Users\\Aleksandr\\Desktop\\Study\\QaGuru\\src\\test\\resources\\example");
+        $("input[type='file']").uploadFile(exampleFile1);
+        $("#file-submit").click();
+        $("#uploaded-files").shouldHave(text("example"));
     }
+
+    @Test
+    @DisplayName("Загрузка файла по относительному пути(рекомендуется)")
+    void fileNameShouldDisplayedAfterUploadActionFromClassPathTest() {
+        open("https://the-internet.herokuapp.com/upload");
+        $("input[type='file']").uploadFromClasspath("example");
+        $("#file-submit").click();
+        $("#uploaded-files").shouldHave(text("example"));
+    }
+
+
+
 
 }
