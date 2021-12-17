f1b26ca8b8f18c483ba7887205f95707cdf7d7d9
Brooklyn92 <Tubanar173russia>
2021-12-17 16:39:29 +0400

    lesson7 part3

--- src/test/java/lesson7/FilesTest.java
+++ src/test/java/lesson7/FilesTest.java
@@ -1,9 +1,12 @@
 package lesson7;
-
+import org.apache.commons.io.IOUtils;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
@@ -28,6 +31,16 @@ public class FilesTest {
         $("input[type='file']").uploadFromClasspath("example");
         $("#file-submit").click();
         $("#uploaded-files").shouldHave(text("example"));
+    }
+    @Test
+    @DisplayName("Сачивание файла перед проверкой его содеражимого")
+    void downloadSimpleTextFileTest() throws IOException {
+        open("https://github.com/junit-team/junit5/blob/main/README.md");
+        File download = $("#raw-url").download(); // метод для скачивания файла.
+        String fileContent = IOUtils.toString(new FileReader(download));// подключаем библиотеку IOUtils для проверки содержимого файла
+        Assertions.assertTrue(fileContent.contains("This repository is the home of the next generation of JUnit, _JUnit 5_."));// проверка условия на то, что искомый текст будет в скачеваемом файле
+        //System.out.println(download.getAbsoluteFile()); - качает файл по абсолютному пути, в throws нужно прописать исклечение FileNotFoundException.
+
     }
 
 
