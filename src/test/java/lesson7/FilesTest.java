3353aa9d284f5bee772ff7b7bdbde8b14a27e867
Brooklyn92 <Tubanar173russia>
2021-12-20 17:59:45 +0400

    lesson7 part4

--- src/test/java/lesson7/FilesTest.java
+++ src/test/java/lesson7/FilesTest.java
@@ -1,16 +1,22 @@
 package lesson7;
+import com.codeborne.pdftest.PDF;
+import com.codeborne.selenide.selector.ByText;
+import com.codeborne.xlstest.XLS;
 import org.apache.commons.io.IOUtils;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class FilesTest {
 
@@ -38,12 +44,35 @@ public class FilesTest {
         open("https://github.com/junit-team/junit5/blob/main/README.md");
         File download = $("#raw-url").download(); // метод для скачивания файла.Можно скачивать файлы только со ссылкой с атрибутом “href”.
         String fileContent = IOUtils.toString(new FileReader(download));// подключаем библиотеку IOUtils для проверки содержимого файла
-        Assertions.assertTrue(fileContent.contains("This repository is the home of the next generation of JUnit, _JUnit 5_."));// проверка условия на то, что искомый текст будет в скачеваемом файле
+        assertTrue(fileContent.contains("This repository is the home of the next generation of JUnit, _JUnit 5_."));// проверка условия на то, что искомый текст будет в скачеваемом файле
         //System.out.println(download.getAbsoluteFile()); - качает файл по абсолютному пути, в throws нужно прописать исклечение FileNotFoundException.
 
     }
 
+    @Test
+    @DisplayName("Скачивание PDF файла")
+    void pdfFileDownloadTest() throws IOException {
+        open("https://junit.org/junit5/docs/current/user-guide/");
+        File pdf = $(byText("PDF download")).download();
+        PDF parsePDF = new PDF(pdf);
+        Assertions.assertEquals(166, parsePDF.numberOfPages);
+    }
 
-
+    @Test
+    @DisplayName("Скачивание XLS файла")
+    void xlsFileDownloadTest() throws IOException {
+        open("http://romashka2008.ru/price");
+        $(".remodal-close-btn").click();
+        File file = $$("a[href*='prajs']")
+                .find(text("Скачать Прайс-лист Excel"))
+                .download();
+        XLS parsedXLS = new XLS(file);
+        boolean checkPassed = parsedXLS.excel.getSheetAt(0)
+                .getRow(11)
+                .getCell(1)
+                .getStringCellValue()
+                .contains("693010, Сахалинская обл, Южно-Сахалинск г, им Анкудинова Федора Степановича б-р, дом № 15, корпус А");
+        assertTrue(checkPassed);
+    }
 
 }
