fa5bb6fa323e90e260883a1e4de869daffed756b
Aleksandr <yurchenko221292@mail.ru>
2021-12-28 19:38:04 +0400

    hw7 final

--- src/test/java/lesson7/home_work/test_pdf_file/TestPDF.java
+++ src/test/java/lesson7/home_work/test_pdf_file/TestPDF.java
@@ -1,4 +1,27 @@
 package lesson7.home_work.test_pdf_file;
 
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.IOException;
+
 public class TestPDF {
+    @DisplayName("Чтение PDF-файла")
+    @Test
+    void testPDFRead() {
+        try {
+            File file = new File("src/test/resources/163902.pdf");
+            PDDocument document = PDDocument.load(file);
+            PDFTextStripper pdfStripper = new PDFTextStripper();
+            String text = pdfStripper.getText(document);
+            Assertions.assertEquals(true, text.contains("600000, Владимирская обл, г Владимир"));
+            document.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/test/java/lesson7/home_work/tets_doc_file/TestDoc.java
+++ src/test/java/lesson7/home_work/tets_doc_file/TestDoc.java
@@ -1,21 +1,8 @@
 package lesson7.home_work.tets_doc_file;
-
-import org.apache.pdfbox.pdmodel.PDDocument;
-import org.apache.pdfbox.text.PDFTextStripper;
-import org.apache.poi.xwpf.model.XWPFHeaderFooterPolicy;
-import org.apache.poi.xwpf.usermodel.ParagraphAlignment;
 import org.apache.poi.xwpf.usermodel.XWPFDocument;
 import org.apache.poi.xwpf.usermodel.XWPFParagraph;
 import org.apache.poi.xwpf.usermodel.XWPFRun;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTP;
-import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTR;
-import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTSectPr;
-import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTText;
-
-import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
@@ -24,105 +11,20 @@ public class TestDoc {
     @Test
     void testDocFile() {
            try {
-               FileOutputStream outputStream = new FileOutputStream("C:/testFile/docArcane.docx");
+               FileOutputStream outputStream = new FileOutputStream("src/test/resources/Arcane22.docx");
                XWPFDocument testDoc = new XWPFDocument();
                XWPFParagraph bodyDoc = testDoc.createParagraph();
                XWPFRun config = bodyDoc.createRun();
                config.setText("Hello Arcane");
                testDoc.write(outputStream);
-                outputStream.close();
-            } catch (IOException e) {
-                e.printStackTrace();
+               outputStream.close();
+            }  catch (IOException e) {
+               e.printStackTrace();
             }
-
-
-       /* try {
-            // создаем модель docx документа,
-            // к которой будем прикручивать наполнение (колонтитулы, текст)
-            XWPFDocument docxModel = new XWPFDocument();
-            CTSectPr ctSectPr = docxModel.getDocument().getBody().addNewSectPr();
-            // получаем экземпляр XWPFHeaderFooterPolicy для работы с колонтитулами
-            XWPFHeaderFooterPolicy headerFooterPolicy = new XWPFHeaderFooterPolicy(docxModel, ctSectPr);
-
-            // создаем верхний колонтитул Word файла
-            CTP ctpHeaderModel = createHeaderModel(
-                    "Верхний колонтитул - создано с помощью Apache POI на Java :)"
-            );
-            // устанавливаем сформированный верхний
-            // колонтитул в модель документа Word
-            XWPFParagraph headerParagraph = new XWPFParagraph(ctpHeaderModel, docxModel);
-            headerFooterPolicy.createHeader(
-                    XWPFHeaderFooterPolicy.DEFAULT,
-                    new XWPFParagraph[]{headerParagraph}
-            );
-
-            // создаем нижний колонтитул docx файла
-            CTP ctpFooterModel = createFooterModel("Просто нижний колонтитул");
-            // устанавливаем сформированый нижний
-            // колонтитул в модель документа Word
-            XWPFParagraph footerParagraph = new XWPFParagraph(ctpFooterModel, docxModel);
-            headerFooterPolicy.createFooter(
-                    XWPFHeaderFooterPolicy.DEFAULT,
-                    new XWPFParagraph[]{footerParagraph}
-            );
-
-            // создаем обычный параграф, который будет расположен слева,
-            // будет синим курсивом со шрифтом 25 размера
-            XWPFParagraph bodyParagraph = docxModel.createParagraph();
-            bodyParagraph.setAlignment(ParagraphAlignment.RIGHT);
-            XWPFRun paragraphConfig = bodyParagraph.createRun();
-            paragraphConfig.setItalic(true);
-            paragraphConfig.setFontSize(25);
-            // HEX цвет без решетки #
-            paragraphConfig.setColor("06357a");
-            paragraphConfig.setText(
-                    "Prologistic.com.ua - новые статьи по Java и Android каждую неделю. Подписывайтесь!"
-            );
-
-            // сохраняем модель docx документа в файл
-            FileOutputStream outputStream = new FileOutputStream("C:/testFile/Arcane.docx");
-            docxModel.write(outputStream);
-            outputStream.close();
-        } catch (Exception e) {
-            e.printStackTrace();
         }
-        System.out.println("Успешно записан в файл");
-    }
-    private static CTP createFooterModel(String footerContent) {
-        // создаем футер или нижний колонтитул
-        CTP ctpFooterModel = CTP.Factory.newInstance();
-        CTR ctrFooterModel = ctpFooterModel.addNewR();
-        CTText cttFooter = ctrFooterModel.addNewT();
 
-        cttFooter.setStringValue(footerContent);
-        return ctpFooterModel;
+
+
     }
 
-    private static CTP createHeaderModel(String headerContent) {
-        // создаем хедер или верхний колонтитул
-        CTP ctpHeaderModel = CTP.Factory.newInstance();
-        CTR ctrHeaderModel = ctpHeaderModel.addNewR();
-        CTText cttHeader = ctrHeaderModel.addNewT();
 
-        cttHeader.setStringValue(headerContent);
-        return ctpHeaderModel;
-    }
-
-        */
-    }
-    @DisplayName("Чтение PDF-файла")
-    @Test
-    void testPDFRead() {
-        try {
-        File file = new File("src/test/resources/163902.pdf");
-        PDDocument document = PDDocument.load(file);
-        PDFTextStripper pdfStripper = new PDFTextStripper();
-        String text = pdfStripper.getText(document);
-        Assertions.assertEquals(true, text.contains("600000, Владимирская обл, г Владимир"));
-        document.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-}
