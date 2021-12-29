5fae8a7ea4b1e354b9ae5f64b638e6856f960c6a
Brooklyn92 <Tubanar173russia>
2021-12-29 09:57:53 +0400

    lesson7 hm part2

--- src/test/java/lesson7/home_work/test_pdf_file/TestPDF.java
+++ src/test/java/lesson7/home_work/test_pdf_file/TestPDF.java
@@ -5,7 +5,6 @@ import org.apache.pdfbox.text.PDFTextStripper;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
 import java.io.File;
 import java.io.IOException;
 
