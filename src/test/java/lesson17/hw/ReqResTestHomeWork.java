46ab5bbdeaeb73ff3237e0c855d1dff4953736e6
Brooklyn92 <Tubanar173russia>
2021-12-30 17:45:18 +0400

    lesson17 hw par2

--- src/test/java/lesson17/hw/ReqResTestHomeWork.java
+++ src/test/java/lesson17/hw/ReqResTestHomeWork.java
@@ -1,12 +1,15 @@
 package lesson17.hw;
 
 import io.restassured.RestAssured;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-
+import static io.restassured.RestAssured.get;
 import static io.restassured.RestAssured.given;
 import static io.restassured.http.ContentType.JSON;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ReqResTestHomeWork {
     @BeforeAll
@@ -26,6 +29,41 @@ public class ReqResTestHomeWork {
                 .statusCode(200)
                 .body("id", is(4))
                 .body("token", is("QpwL5tke4Pnpja7X4"));
-
     }
+    @Test
+    void create() {
+        String data = "{ \"name\": \"morpheus\", \"job\": \"leader\" }";
+        given()
+                .contentType(JSON)
+                .body(data)
+                .when()
+                .post("api/users")
+                .then()
+                .statusCode(201)
+                .body("name", is("morpheus"))
+                .body("job", is("leader"));
+    }
+
+    @Test
+    void singleResource() {
+        Integer response =
+                get("api/unknown/2")
+                        .then()
+                        .statusCode(200)
+                        .extract()
+                        .path("data.id");
+        assertThat(response).isEqualTo(2);
+    }
+
+    @Test
+    void singleUserNotFound() {
+        Response response =
+        get("api/users/23")
+                .then()
+                .statusCode(404)
+                .extract()
+                .response();
+        assertEquals("{}", response.asString());
+    }
+
 }
