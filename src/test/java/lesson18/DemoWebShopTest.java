c73560cd8ba4c2eb9d519f50b3b0191bbb85352c
Aleksandr <yurchenko221292@mail.ru>
2022-01-09 22:56:33 +0400

    lesson18

--- src/test/java/lesson18/DemoWebShopTest.java
+++ src/test/java/lesson18/DemoWebShopTest.java
@@ -4,7 +4,6 @@ import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import io.restassured.response.Response;
-
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.is;
 
